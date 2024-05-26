413f1c5119a4a957d4b58bfc3fdbd64c9c9689c1
Phil Schumann <metaleap@users.noreply.github.com>
2024-05-26 09:35:32 +0200

    More flut explorings

--- guis/flut/lib/main.dart
+++ guis/flut/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+const appWindowTitle = "App window title here";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WindowManager.instance.ensureInitialized();
+  await windowManager.setTitle(appWindowTitle);
   runApp(const MainApp());
 }
 
@@ -9,11 +15,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: appWindowTitle,
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          leading: const IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'App nav',
+            onPressed: null,
+          ),
+          title: const Text(appWindowTitle),
+          actions: [
+            const IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+            IconButton(
+              icon: const Icon(Icons.auto_delete_rounded),
+              tooltip: 'Wot',
+              onPressed: () {
+                print("Wot!");
+              },
+            ),
+          ],
+        ),
+        body: const Center(
           child: Text('Hello World!'),
         ),
+        bottomNavigationBar: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.apple), label: "Foo 1", tooltip: "Tooltip1"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.window), label: "Foo 2", tooltip: "Tooltip2"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.android), label: "Foo 3", tooltip: "Tooltip3"),
+        ]),
       ),
     );
   }
